30de4f3333d743c361e4d9243e9bad7a4e1e8a13
nususu1004 <nususu1004@gmail.com>
2025-07-08 15:20:29 +0900

    20250728

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <footer class="py-5">
+    <div class="container">
+      <div>&copy; 2024. Example. All right reserved.</div>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/services/accountService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8080/api/v1/account";
+
+export const join = (args) => {
+  return axios.post("/join", args).catch((e) => e.response);
+};
+
+export const login = (args) => {
+  return axios.post("/login", args).catch((e) => e.response);
+};
